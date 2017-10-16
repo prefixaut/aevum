c227fde5a20191a78c252b859ef611064451c6d4
PreFiXAUT <me@prefixaut.net>
2017-10-16 02:23:12 +0200

    TypeScript re-write and tokenzier re-write

---
+++ src/compiler.ts
@@ -0,0 +1,58 @@
+import { Token } from './tokenizer'
+
+export function compile(tokens: Array<string|Token>) {
+    const h = new Array<string|Token>()
+    const m = new Array<string|Token>()
+    const s = new Array<string|Token>()
+    const d = new Array<string|Token>()
+
+    function push(value: string|Token, ...arrs: Array<Array<string|Token>>) {
+        arrs.forEach((arr) => {
+            arr.push(value)
+        })
+    }
+
+    const length = tokens.length
+    for (let i = 0; i < length; i++) {
+        const t = tokens[i]
+
+        if (typeof t === 'string') {
+            push(t, h, m, s, d)
+            continue
+        }
+
+        if (typeof t !== 'object' || t === null || Array.isArray(t)) {
+            throw new TypeError('Invalid token "' + t + '"!')
+        }
+
+        if (!t.optional) {
+            push(t, h, m, s, d)
+            continue
+        }
+
+        const arrs = new Array<Array<string|Token>>()
+        switch (t.type) {
+            case 'd':
+                arrs.push(d)
+            case 's':
+                arrs.push(s)
+            case 'm':
+                arrs.push(m)
+            case 'h':
+                arrs.push(h)
+        }
+
+        const formatTokens = (t.hasOwnProperty('format') && t.format != null) ? t.format : [t]
+        const formatLength = formatTokens.length
+        for (let k = 0; k < formatLength; k++) {
+            push(formatTokens[k], ...arrs)
+        }
+    }
+
+    return {
+        h,
+        m,
+        s,
+        d,
+    }
+}
