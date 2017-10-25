66389ff4a8b9c22e3e1f3daa0b6d1daa9387de9d
PreFiXAUT <me@prefixaut.net>
2017-10-26 01:01:21 +0200

    Updated unit-tests

--- test/tests.js
+++ test/tests.js
@@ -13,84 +13,94 @@ describe('aevum', function() {
     describe('format', function() {
 
         it('should compile', function() {
-            expect(function() {
-                new aevum('[d]')
-                new aevum('[dd]')
-                new aevum('[ddd]')
-                new aevum('[d1]')
-                new aevum('[d2]')
-                new aevum('[d3]')
-
-                new aevum('[s]')
-                new aevum('[ss]')
-                new aevum('[s1]')
-                new aevum('[s2]')
-
-                new aevum('[m]')
-                new aevum('[mm]')
-                new aevum('[m1]')
-                new aevum('[m2]')
-
-                new aevum('[h]')
-                new aevum('[hh]')
-                new aevum('[h1]')
-                new aevum('[h2]')
-                new aevum('[h10000]')
-            }).to.not.throw(SyntaxError)
+            [
+                '[d]',
+                '[dd]',
+                '[ddd]',
+                '[d1]',
+                '[d2]',
+                '[d3]',
+                '[s]',
+                '[ss]',
+                '[s1]',
+                '[s2]',
+                '[m]',
+                '[mm]',
+                '[m1]',
+                '[m2]',
+                '[h]',
+                '[hh]',
+                '[h1]',
+                '[h2]',
+                '[h10000]',
+                'hello',
+                '(d)[content]',
+                '(d)[other[mm]content]',
+                'hello\\[dude\\]',
+                '(d)[hello\\[dude\\]]',
+                'something(d)[here]',
+            ].forEach(format => {
+                expect(function() {
+                    new aevum(format)
+                }).to.not.throw(SyntaxError)
+            })
         })
 
         it('should throw syntax errors', function() {
-            expect(function() {
-                new aevum('(should-fail)')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('(dd)')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('(h\\hh)[test]')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('(dd)[')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('(mm)[[[ss]]]')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('(mm)[[invalid]]')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('(mm)[[test]')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('[dddd]')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('[d4]')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('[sss]')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('[s3]')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('[mmm]')
-            }).to.throw(SyntaxError)
-
-            expect(function() {
-                new aevum('[m3]')
-            }).to.throw(SyntaxError)
+            [
+                '(should-fail)',
+                '(dd)',
+                '(h\\hh)[test]',
+                '(dd)[',
+                '(dd)hello',
+                'there()is content',
+                '(ab)',
+                '(mm)[[[ss]]]',
+                '(mm)[[invalid]]',
+                '(mm)[[test]',
+                '[dddd]',
+                '[d4]',
+                '[sss]',
+                '[s3]',
+                '[mmm]',
+                '[m3]',
+                '[a]',
+                '[b]',
+                '[c]',
+                '[e]',
+                '[f]',
+                '[g]',
+                '[k]',
+                '[l]',
+                '[n]',
+                '[o]',
+                '[p]',
+                '[q]',
+                '[r]',
+                '[t]',
+                '[u]',
+                '[v]',
+                '[x]',
+                '[y]',
+                '[z]',
+                '[]',
+                '(d)[bla[]]',
+                '[da]',
+                '[sa]',
+                '[ma]',
+                '[ha]',
+                '[d500]',
+                '[dd34]',
+                '[d24de]',
+                '(d)[[da]]',
+                '(d)[[sa]]',
+                '(d)[[ma]]',
+                '(d)[[ha]]',
+            ].forEach(format => {
+                expect(function() {
+                    new aevum(format)
+                }).to.throw(SyntaxError)
+            })
         })
 
         it('should be an object', function() {
@@ -101,19 +111,124 @@ describe('aevum', function() {
             expect(new aevum('something')).to.have.property('format').and.to.be.an('function')
         })
 
-        it('should format', function() {
-            var format = new aevum('(hh)[Its very late! #h [m2]m!][mm]m, [ss]s, [d3]ms cool')
-            expect(function() {
-                format.format(1000)
-            }).to.not.throw()
+        it('should format correctly', function() {
+            [
+                {
+                    type: 'd',
+                    empty: [
+                        0,
+                        {}
+                    ],
+                    filled: [
+                        1,
+                        { milliseconds: 1 },
+                        { seconds: 1 },
+                        { minutes: 1 },
+                        { hours: 1 },
+                    ]
+                },
+                {
+                    type: 's',
+                    empty: [
+                        0,
+                        {},
+                        999,
+                        { milliseconds: 999 },
+                        { seconds: 0 },
+                    ],
+                    filled: [
+                        1000,
+                        { seconds: 1 },
+                        { minutes: 1 },
+                        { hours: 1 },
+                    ]
+                },
+                {
+                    type: 'm',
+                    empty: [
+                        0,
+                        {},
+                        59999,
+                        { milliseconds: 999 },
+                        { seconds: 59 },
+                    ],
+                    filled: [
+                        60000,
+                        { minutes: 1 },
+                        { hours: 1 },
+                    ]
+                },
+                {
+                    type: 'h',
+                    empty: [
+                        0,
+                        {},
+                        3599999,
+                        { milliseconds: 999 },
+                        { seconds: 59 },
+                        { minutes: 59 },
+                    ],
+                    filled: [
+                        3600000,
+                        { hours: 1 },
+                    ]
+                }
+            ].forEach(obj => {
+                obj.empty.forEach(time => {
+                    expect(new aevum('(' + obj.type + ')[test]').format(time)).to.be.equal('')
+                })
+                obj.filled.forEach(time => {
+                    expect(new aevum('(' + obj.type + ')[test]').format(time)).to.be.equal('test')
+                })
+            })
+        })
 
-            expect(format.format(1000)).to.be.equal('00m, 01s, 000ms cool')
+        it('should format padding correctly', function() {
+            [
+                {
+                    type: 's',
+                    below: ['d'],
+                    time: { seconds: 1 },
+                },
+                {
+                    type: 'm',
+                    below: ['d', 's'],
+                    time:  { minutes: 1 },
+                },
+                {
+                    type: 'h',
+                    below: ['d', 's', 'm'],
+                    time: { hours: 1 },
+                }
+            ].forEach(obj => {
+                obj.below.forEach(b => {
+                    const tmp = new aevum('(' + obj.type + ')[[' + b + ']]')
+                    expect(tmp.format(obj.time, true)).to.be.equal(b === 'd' ? '000' : '00')
+                    expect(tmp.format(obj.time)).to.be.equal('0')
+                })
+            })
+        })
 
-            expect(format.format({
-                seconds: 1
-            })).to.be.equal('00m, 01s, 000ms cool')
-
-            expect(format.format(3600000)).to.be.equal('Its very late! 01h 00m!00m, 00s, 000ms cool')
+        it('should throw format errors', function() {
+            const format = new aevum('something');
+            [
+                undefined,
+                null,
+                'test',
+                NaN,
+                Infinity,
+                -NaN,
+                -Infinity,
+                ['whatever'],
+                true,
+                false,
+                -1,
+                -2357654
+            ].forEach(invalid => {
+                expect(function() {
+                    format.format(invalid)
+                }).to.throw(TypeError)
+            })
         })
     })
 })
