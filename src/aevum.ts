9aeaa2eb4799fc7b0f27b823fbe02f1bbbdb273e
PreFiXAUT <me@prefixaut.net>
2017-11-02 18:04:16 +0100

    updated tslint configuration

--- src/aevum.ts
+++ src/aevum.ts
@@ -1,11 +1,11 @@
-import { compile } from './compiler'
-import { Token, tokenize } from './tokenizer'
+import { compile } from './compiler';
+import { Token, tokenize } from './tokenizer';
 
 export interface Time {
-    hours?: number
-    minutes?: number
-    seconds?: number
-    milliseconds?: number
+    hours?: number;
+    minutes?: number;
+    seconds?: number;
+    milliseconds?: number;
 }
 
 export const TimeTypes: { [key: string]: number } = {
@@ -13,65 +13,65 @@ export const TimeTypes: { [key: string]: number } = {
     m: 2,
     s: 2,
     d: 3,
-}
+};
 
 export class Aevum {
 
-    private tokens: Array<string | Token>
-    private compiled: { [key: string]: Array<string | Token> }
+    private tokens: Array<string | Token>;
+    private compiled: { [key: string]: Array<string | Token> };
 
     constructor(
         private formatString: string,
     ) {
-        this.tokens = tokenize(formatString)
-        this.compiled = compile(this.tokens)
+        this.tokens = tokenize(formatString);
+        this.compiled = compile(this.tokens);
     }
 
     public format(time: number | object, performPadding: boolean = false): string {
-        const timeObj = this.asTime(time)
-        let build = ''
-        let arr: Array<string | Token>
+        const timeObj = this.asTime(time);
+        let build = '';
+        let arr: Array<string | Token>;
 
         if (timeObj.hours && timeObj.hours > 0) {
-            arr = this.compiled.h
+            arr = this.compiled.h;
         } else if (timeObj.minutes && timeObj.minutes > 0) {
-            arr = this.compiled.m
+            arr = this.compiled.m;
         } else if (timeObj.seconds && timeObj.seconds > 0) {
-            arr = this.compiled.s
+            arr = this.compiled.s;
         } else if (timeObj.milliseconds && timeObj.milliseconds > 0) {
-            arr = this.compiled.d
+            arr = this.compiled.d;
         } else {
-            return ''
+            return '';
         }
 
-        const len = arr.length
+        const len = arr.length;
         for (let i = 0; i < len; i++) {
-            const part = arr[i]
+            const part = arr[i];
 
             if (typeof part === 'string') {
-                build += part
-                continue
+                build += part;
+                continue;
             }
 
-            build += this.formatTimePart(part.type, part.length, timeObj, performPadding)
+            build += this.formatTimePart(part.type, part.length, timeObj, performPadding);
         }
 
-        return build
+        return build;
     }
 
     private asTime(data: number | object): Time {
         if (typeof data === 'undefined' || data === null) {
-            throw new TypeError('The time may not be null/undefined!')
+            throw new TypeError('The time may not be null/undefined!');
         }
         if (typeof data === 'number') {
             if (isNaN(data)) {
-                throw new TypeError('The time may not be NaN!')
+                throw new TypeError('The time may not be NaN!');
             }
             if (!isFinite(data)) {
-                throw new TypeError('The number has to be finite!')
+                throw new TypeError('The number has to be finite!');
             }
             if (data < 0) {
-                throw new TypeError('The number has to be positive!')
+                throw new TypeError('The number has to be positive!');
             }
             // Parse an timeing object from the number
             return {
@@ -79,62 +79,62 @@ export class Aevum {
                 minutes: ((data / 60000) | 0) % 60,
                 seconds: ((data / 1000) | 0) % 60,
                 milliseconds: data % 1000,
-            }
+            };
         }
         if (typeof data === 'object') {
             if (Array.isArray(data)) {
-                throw new TypeError('The time may not be an array!')
+                throw new TypeError('The time may not be an array!');
             }
             return Object.assign({
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
                 milliseconds: 0,
-            }, data)
+            }, data);
         }
-        throw new TypeError(`Invalid type "${typeof data}"!`)
+        throw new TypeError(`Invalid type "${typeof data}"!`);
     }
 
     private formatTimePart(type: string, length: number, time: Time, padding: boolean) {
-        let value = 0
+        let value = 0;
 
         switch (type) {
             case 'h':
-                value = time.hours || 0
-                break
+                value = time.hours || 0;
+                break;
             case 'm':
-                value = time.minutes || 0
-                break
+                value = time.minutes || 0;
+                break;
             case 's':
-                value = time.seconds || 0
-                break
+                value = time.seconds || 0;
+                break;
             case 'd':
-                value = time.milliseconds || 0
-                break
+                value = time.milliseconds || 0;
+                break;
         }
 
         if (padding) {
-            let tmp = 0
+            let tmp = 0;
             switch (type) {
                 case 'd':
                     if (tmp === 0) {
-                        tmp = time.seconds || tmp
+                        tmp = time.seconds || tmp;
                     }
                 case 's':
                     if (tmp === 0) {
-                        tmp = time.minutes || tmp
+                        tmp = time.minutes || tmp;
                     }
                 case 'm':
                     if (tmp === 0) {
-                        tmp = time.hours || tmp
+                        tmp = time.hours || tmp;
                     }
             }
             if (tmp > 0) {
-                length = TimeTypes[type]
+                length = TimeTypes[type];
             }
         }
 
-        const str = value.toString()
-        return '0'.repeat(length - str.length) + str
+        const str = value.toString();
+        return '0'.repeat(length - str.length) + str;
     }
 }
--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,51 +1,51 @@
-import { TimeTypes } from './aevum'
+import { TimeTypes } from './aevum';
 
 export interface Token {
-    type: string,
-    length: number,
-    optional: boolean,
-    format?: Array<(string | Token)>,
+    type: string;
+    length: number;
+    optional: boolean;
+    format?: Array<(string | Token)>;
 }
 
-type State = 'STRING' | 'ESCAPED' | 'IN-OPTIONAL' | 'REQUIRE-FORMAT' | 'IN-FORMAT' | 'IN-NESTED'
+type State = 'STRING' | 'ESCAPED' | 'IN-OPTIONAL' | 'REQUIRE-FORMAT' | 'IN-FORMAT' | 'IN-NESTED';
 
-const numberRegex = /^\d+$/
+const numberRegex = /^\d+$/;
 
 export function tokenize(format: string): Array<(string | Token)> {
 
-    let oldState = 'STRING' as State
-    let state = 'STRING' as State
+    let oldState = 'STRING' as State;
+    let state = 'STRING' as State;
 
     // Regex used to replace hashes with the format
-    const hashRegex = /([^\\])(#)/
+    const hashRegex = /([^\\])(#)/;
 
     // Length of the format-string
-    const len = format.length
+    const len = format.length;
 
     // The nested-format content
-    let nestedBuild = ''
+    let nestedBuild = '';
     // The current string build
-    let build = ''
+    let build = '';
 
     // Type of the current format that is being processed
-    let type: string = ''
-    let length: number = 0
+    let type: string = '';
+    let length: number = 0;
     // If the format is optional or not
-    let optional: boolean = false
+    let optional: boolean = false;
 
     // The compiled array of items
-    const tokens = new Array<(Token | string)>()
+    const tokens = new Array<(Token | string)>();
 
     for (let i = 0; i < len; i++) {
-        const c = format.charAt(i)
+        const c = format.charAt(i);
 
         switch (state) {
             case 'REQUIRE-FORMAT':
                 if (c !== '[') {
-                    throw new SyntaxError(`Expected a format assignment at position: ${i}! Got "${c}" instead!`)
+                    throw new SyntaxError(`Expected a format assignment at position: ${i}! Got "${c}" instead!`);
                 }
-                state = 'IN-FORMAT'
-                break
+                state = 'IN-FORMAT';
+                break;
 
             case 'ESCAPED':
                 // Adding the backslash/escape character again, since the content
@@ -53,191 +53,191 @@ export function tokenize(format: string): Array<(string | Token)> {
                 // content not to be escaped properly, as the escape doesn't
                 // happen in the second round of the tokenizing
                 if (oldState === 'IN-FORMAT') {
-                    build += '\\'
+                    build += '\\';
                 }
-                state = oldState
-                build += c
-                break
+                state = oldState;
+                build += c;
+                break;
 
             case 'STRING':
                 if (c === '\\') {
-                    oldState = state
-                    state = 'ESCAPED'
-                    break
+                    oldState = state;
+                    state = 'ESCAPED';
+                    break;
                 }
 
                 if (c === '(') {
-                    state = 'IN-OPTIONAL'
+                    state = 'IN-OPTIONAL';
                     if (build !== '') {
-                        tokens.push(build)
+                        tokens.push(build);
                     }
-                    build = ''
-                    break
+                    build = '';
+                    break;
                 }
 
                 if (c === '[') {
-                    state = 'IN-FORMAT'
+                    state = 'IN-FORMAT';
                     if (build !== '') {
-                        tokens.push(build)
+                        tokens.push(build);
                     }
-                    build = ''
-                    break
+                    build = '';
+                    break;
                 }
 
-                build += c
-                break
+                build += c;
+                break;
 
             case 'IN-OPTIONAL':
                 if (c === ')') {
                     if (build.length === 0) {
                         throw new SyntaxError(
-                            `Invalid Syntax on position ${i}! Closed optional defintion without setting a type!`)
+                            `Invalid Syntax on position ${i}! Closed optional defintion without setting a type!`);
                     }
 
                     if (!TimeTypes.hasOwnProperty(build[0])) {
-                        throw new SyntaxError(`Unknown Type '${build[0]}' on position ${i}!`)
+                        throw new SyntaxError(`Unknown Type '${build[0]}' on position ${i}!`);
                     }
 
                     if (build.length > 1) {
-                        length = verifyType(build.toLowerCase(), i)
+                        length = verifyType(build.toLowerCase(), i);
                     } else {
-                        length = 1
+                        length = 1;
                     }
 
-                    type = build[0]
-                    build = ''
-                    state = 'REQUIRE-FORMAT'
-                    optional = true
-                    break
+                    type = build[0];
+                    build = '';
+                    state = 'REQUIRE-FORMAT';
+                    optional = true;
+                    break;
                 }
 
-                build += c
-                break
+                build += c;
+                break;
 
             case 'IN-FORMAT':
                 if (c === '\\') {
-                    oldState = state
-                    state = 'ESCAPED'
-                    break
+                    oldState = state;
+                    state = 'ESCAPED';
+                    break;
                 }
 
                 if (c === '[') {
-                    state = 'IN-NESTED'
-                    break
+                    state = 'IN-NESTED';
+                    break;
                 }
 
                 if (c === ']') {
                     if (!optional) {
                         if (build.length === 0) {
-                            throw new SyntaxError(`Invalid Syntax on position ${i}! An format-block may not be empty!`)
+                            throw new SyntaxError(`Invalid Syntax on position ${i}! An format-block may not be empty!`);
                         }
 
                         if (!TimeTypes.hasOwnProperty(build[0])) {
-                            throw new SyntaxError(`Unknown Type '${build[0]}' on position ${i}!`)
+                            throw new SyntaxError(`Unknown Type '${build[0]}' on position ${i}!`);
                         }
 
                         if (build.length > 1) {
-                            length = verifyType(build.toLowerCase(), i)
+                            length = verifyType(build.toLowerCase(), i);
                         } else {
-                            length = 1
+                            length = 1;
                         }
 
-                        type = build[0]
+                        type = build[0];
                     }
 
                     const token: Token = {
                         type,
                         length,
                         optional,
-                    }
+                    };
 
                     if (optional) {
-                        token.format = tokenize(build.replace(hashRegex, `$1[${type}${length}]`))
+                        token.format = tokenize(build.replace(hashRegex, `$1[${type}${length}]`));
                     }
 
-                    tokens.push(token)
-                    build = ''
-                    state = 'STRING'
-                    optional = false
-                    break
+                    tokens.push(token);
+                    build = '';
+                    state = 'STRING';
+                    optional = false;
+                    break;
                 }
 
-                build += c
-                break
+                build += c;
+                break;
 
             case 'IN-NESTED':
                 if (c === '[') {
-                    throw new SyntaxError(`Invalid Syntax on position ${i}! You cannot nest an already nested format!`)
+                    throw new SyntaxError(`Invalid Syntax on position ${i}! You cannot nest an already nested format!`);
                 }
                 if (c === ']') {
                     if (nestedBuild.length === 0) {
-                        throw new SyntaxError(`Invalid Syntax on position ${i}! An format-block may not be empty!`)
+                        throw new SyntaxError(`Invalid Syntax on position ${i}! An format-block may not be empty!`);
                     }
 
                     if (!TimeTypes.hasOwnProperty(nestedBuild[0])) {
-                        throw new SyntaxError(`Unknown Type '${nestedBuild[0]}' on position ${i}!`)
+                        throw new SyntaxError(`Unknown Type '${nestedBuild[0]}' on position ${i}!`);
                     }
 
-                    verifyType(nestedBuild.toLowerCase(), i)
+                    verifyType(nestedBuild.toLowerCase(), i);
 
-                    build += `[${nestedBuild}]`
-                    nestedBuild = ''
-                    state = 'IN-FORMAT'
-                    break
+                    build += `[${nestedBuild}]`;
+                    nestedBuild = '';
+                    state = 'IN-FORMAT';
+                    break;
                 }
 
-                nestedBuild += c
-                break
+                nestedBuild += c;
+                break;
         }
     }
 
     if (state !== 'STRING') {
-        throw new SyntaxError('Invalid Syntax! Unexpected end of format!')
+        throw new SyntaxError('Invalid Syntax! Unexpected end of format!');
     }
 
     if (build !== '') {
-        tokens.push(build)
+        tokens.push(build);
     }
 
-    return tokens
+    return tokens;
 }
 
 function verifyType(input: string, position: number): number {
-    const type = input[0]
-    let isNumber = false
-    let build = ''
-    let length = 1
+    const type = input[0];
+    let isNumber = false;
+    let build = '';
+    let length = 1;
 
     for (let i = 1; i < input.length; i++) {
-        const c = input[i]
+        const c = input[i];
 
         if (i === 1) {
-            isNumber = numberRegex.test(c)
+            isNumber = numberRegex.test(c);
         }
 
         if (isNumber) {
             if (!numberRegex.test(c)) {
                 throw new SyntaxError(`Invalid Type-Definition on position ${position + i}!` +
-                    `You may not mix the length with characters!`)
+                    `You may not mix the length with characters!`);
             }
-            build += c
+            build += c;
         } else if (c !== type) {
             throw new SyntaxError(`Invalid Type-Defintion on position ${position + i}!` +
-                `You may not mix the types!`)
+                `You may not mix the types!`);
         }
 
-        length++
+        length++;
     }
 
     if (isNumber) {
-        length = parseInt(build, 10)
+        length = parseInt(build, 10);
     }
 
-    const max = TimeTypes[type]
+    const max = TimeTypes[type];
     if (max > 0 && length > max) {
         throw new SyntaxError(`Invalid Type-Definition on position ${position}!` +
-            `The given length is bigger than allowed! Length: ${length}, Max: ${max}`)
+            `The given length is bigger than allowed! Length: ${length}, Max: ${max}`);
     }
 
-    return length
+    return length;
 }
